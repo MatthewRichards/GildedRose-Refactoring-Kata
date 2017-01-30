0e5c81e552feaa940766d814140b3e50c0f03e24
Matthew Richards <Matthew.Richards@softwire.com>
2017-01-30 18:18:26 +0000

    Successfully failing test!

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -1,18 +1,18 @@
-using System;
 using NUnit.Framework;
 using System.Collections.Generic;
 
 namespace GildedRose
 {
-	[TestFixture()]
+	[TestFixture]
 	public class GildedRoseTest
 	{
-		[Test()]
-		public void foo() {
-			IList<Item> Items = new List<Item> { new Item{Name = "foo", SellIn = 0, Quality = 0} };
-			GildedRose app = new GildedRose(Items);
+		[Test]
+		public void foo()
+    {
+			IList<Item> items = new List<Item> { new Item{Name = "foo", SellIn = 0, Quality = 0} };
+			GildedRose app = new GildedRose(items);
 			app.UpdateQuality();
-			Assert.AreEqual("fixme", Items[0].Name);
+			Assert.AreEqual("fixme", items[0].Name);
 		}
 	}
 }
---
+++ csharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GildedRose
+{
+  class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Can't do that, soz");
+    }
+  }
+}
