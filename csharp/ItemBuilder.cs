231fe5d08466548a7c3e364361719e320d35092d
Matthew Richards <Matthew.Richards@softwire.com>
2017-01-30 18:25:29 +0000

    First passing test

---
+++ csharp/ItemBuilder.cs
@@ -0,0 +1,37 @@
+﻿namespace GildedRose
+{
+  public class ItemBuilder
+  {
+    private int? SellIn;
+    private int? Quality;
+    private string Name;
+
+    public ItemBuilder WithSellIn(int value)
+    {
+      SellIn = value;
+      return this;
+    }
+
+    public ItemBuilder WithQuality(int value)
+    {
+      Quality = value;
+      return this;
+    }
+
+    public ItemBuilder WithName(string value)
+    {
+      Name = value;
+      return this;
+    }
+
+    public Item Build()
+    {
+      return new Item
+      {
+        Name = Name ?? "Arbitrary Item",
+        Quality = Quality ?? 17,
+        SellIn = SellIn ?? 42
+      };
+    }
+  }
+}
