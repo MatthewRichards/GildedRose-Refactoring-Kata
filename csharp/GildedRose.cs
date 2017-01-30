2144c6cee865fd5bb5d48f7150fa8ab140c1666f
Matthew Richards <Matthew.Richards@softwire.com>
2017-01-30 19:44:39 +0000

    Improve initial block of quality rules

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace GildedRose
@@ -15,48 +16,32 @@ namespace GildedRose
 		{
 			this.items = items;
 		}
-		
-		public void UpdateQuality()
+
+	  private readonly Dictionary<string, Func<int, int, int>> qualityRules = new Dictionary<string, Func<int, int, int>>
+	  {
+	    {AgedBrie, (sellIn, quality) => quality + 1},
+	    {
+	      BackstagePasses, (sellIn, quality) =>
+	      {
+	        if (sellIn < 6) return quality + 3;
+	        if (sellIn < 11) return quality + 2;
+	        return quality + 1;
+	      }
+	    },
+      {SulfurasHandOfRagnaros, (sellIn, quality) => quality }
+	  };
+
+	  private readonly Func<int, int, int> defaultQualityRule = (sellIn, quality) => quality - 1;
+
+
+    public void UpdateQuality()
 		{
 		  foreach (Item item in items)
 		  {
-		    if (item.Name != AgedBrie && item.Name != BackstagePasses)
-		    {
-		      if (item.Quality > 0)
-		      {
-		        if (item.Name != SulfurasHandOfRagnaros)
-		        {
-		          item.Quality = item.Quality - 1;
-		        }
-		      }
-		    }
-		    else
-		    {
-		      if (item.Quality < 50)
-		      {
-		        item.Quality = item.Quality + 1;
-						
-		        if (item.Name == BackstagePasses)
-		        {
-		          if (item.SellIn < 11)
-		          {
-		            if (item.Quality < 50)
-		            {
-		              item.Quality = item.Quality + 1;
-		            }
-		          }
-							
-		          if (item.SellIn < 6)
-		          {
-		            if (item.Quality < 50)
-		            {
-		              item.Quality = item.Quality + 1;
-		            }
-		          }
-		        }
-		      }
-		    }
-				
+		    var qualityRule = qualityRules.ContainsKey(item.Name) ? qualityRules[item.Name] : defaultQualityRule;
+		    item.Quality = EnforceQualityRange(qualityRule(item.SellIn, item.Quality));
+        
+        
 		    if (item.Name != SulfurasHandOfRagnaros)
 		    {
 		      item.SellIn = item.SellIn - 1;
@@ -101,7 +86,11 @@ namespace GildedRose
 		    }
 		  }
 		}
-		
+
+	  private int EnforceQualityRange(int quality)
+	  {
+	    return Math.Min(50, Math.Max(0, quality));
+	  }
 	}
 	
 	public class Item
